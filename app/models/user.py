688dc7d029fc1fcf8ffbb469dd12c7a0dd3b670f
IIH <ds5105119@gmail.com>
2024-11-27 00:23:40 +0900

    make models

--- app/models/user.py
+++ app/models/user.py
@@ -1,4 +1,62 @@
-from fastapi import Depends
-from sqlalchemy.ext.asyncio import AsyncSession
+import datetime
+from typing import List, Optional
 
-from app.db import Base, get_db
+from sqlalchemy import (
+    JSONB,
+    Boolean,
+    DateTime,
+    ForeignKeyConstraint,
+    Index,
+    Integer,
+    PrimaryKeyConstraint,
+    String,
+    func,
+)
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from .base import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+    __table_args__ = (
+        PrimaryKeyConstraint("id", name="users_pk"),
+        Index("ix_users_email", "email", unique=True),
+        Index("ix_users_handle", "handle", unique=True),
+        Index("ix_users_is_active", "is_active"),
+        Index("ix_users_is_deleted", "is_deleted"),
+    )
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+
+    email: Mapped[str] = mapped_column(String(255))
+    handle: Mapped[str] = mapped_column(String(255))
+    password: Mapped[str] = mapped_column(String(255))
+
+    created_at: Mapped[datetime.datetime] = mapped_column(DateTime, server_default=func.now())
+    updated_at: Mapped[datetime.datetime] = mapped_column(DateTime, onupdate=func.now())
+    deleted_at: Mapped[Optional[datetime.datetime]] = mapped_column(DateTime)
+
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+    is_superuser: Mapped[bool] = mapped_column(Boolean, default=False)
+    is_deleted: Mapped[bool] = mapped_column(Boolean, default=False)
+
+    resignation_reason: Mapped[Optional[str]] = mapped_column(String(255))
+
+    profiles: Mapped["Profile"] = relationship("Profile", back_populates="users", uselist=False)
+
+
+class Profile(Base):
+    __tablename__ = "profiles"
+    __table_args__ = (
+        PrimaryKeyConstraint("id", name="profiles_pk"),
+        ForeignKeyConstraint(["user_id"], ["users.id"], name="profiles_user_id_fk", ondelete="CASCADE"),
+    )
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    user_id: Mapped[int] = mapped_column(Integer, nullable=True)
+
+    profile: Mapped[str] = mapped_column(String(255))
+    bio: Mapped[Optional[str]] = mapped_column(String(255))
+
+    users: Mapped["User"] = relationship("User", back_populates="profiles")
--- app/repository/base.py
+++ app/repository/base.py
@@ -1,8 +1,7 @@
-from sqlite3 import IntegrityError
-from typing import Any, Generic, Iterable, Sequence, Type, TypeVar, cast
+from typing import Any, Sequence, TypeVar, cast
 
-from sqlalchemy import UUID, Column, Label, MetaData, Result, Row, delete, func, insert, select, update
-from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import Column, Label, Result, Row, delete, func, insert, select, update
+from sqlalchemy.ext.asyncio import AsyncSession as Session
 from sqlalchemy.orm import DeclarativeBase
 
 T = TypeVar("T", bound=DeclarativeBase)
@@ -11,7 +10,7 @@ _P = Result[tuple[Any]]
 
 class BaseRepository[T]:
     model: type[T]
-    session: AsyncSession
+    session: Session
 
     def __init__(self, model: type[T]):
         self.model = model
@@ -23,36 +22,36 @@ class BaseRepository[T]:
 
 
 class BaseCreateRepository[T](BaseRepository[T]):
-    async def _create(self, session: AsyncSession, **kwargs: Any) -> T:
+    async def _create(self, session: Session, **kwargs: Any) -> T:
         session.add(entity := self.model(**kwargs))
         await session.commit()
         await session.refresh(entity)
 
         return entity
 
-    async def _bulk_create(self, session: AsyncSession, kwargs: Sequence[dict[str, Any]]) -> None:
+    async def _bulk_create(self, session: Session, kwargs: Sequence[dict[str, Any]]) -> None:
         stmt = insert(self.model).values(kwargs)
         await session.execute(stmt)
         await session.commit()
 
-    async def create(self, session: AsyncSession, *args: Any, **kwargs: Any) -> T | None:
-        if args:
-            return await self._bulk_create(session, list(args))
-        elif kwargs:
+    async def create(self, session: Session, values: Sequence[dict[str, Any]] | None = None, **kwargs: Any) -> T | None:
+        if kwargs:
             return await self._create(session, **kwargs)
+        elif values:
+            return await self._bulk_create(session, values)
         else:
             raise ValueError("Invalid arguments for creation.")
 
 
 class BaseReadRepository[T](BaseRepository[T]):
-    async def get(self, session: AsyncSession, id: int | str, columns: list[str] | None = None) -> _P:
+    async def get(self, session: Session, id: int | str, columns: list[str] | None = None) -> _P:
         columns = self.get_columns(columns)
         stmt = select(*columns).where(cast("ColumnElement[bool]", self.model.id == id))
         result = await session.execute(stmt)
 
         return result
 
-    async def filter_get(self, session: AsyncSession, filters: list, columns: list[str] | None = None) -> _P:
+    async def filter_get(self, session: Session, filters: list, columns: list[str] | None = None) -> _P:
         columns = self.get_columns(columns)
         stmt = select(*columns).where(*filters)
         result = await session.execute(stmt)
@@ -61,24 +60,24 @@ class BaseReadRepository[T](BaseRepository[T]):
 
 
 class BaseUpdateRepository[T](BaseRepository[T]):
-    async def update(self, db: AsyncSession, id: int | str, **kwargs) -> None:
+    async def update(self, db: Session, id: int | str, **kwargs) -> None:
         query = update(self.model).where(cast("ColumnElement[bool]", self.model.id == id)).values(**kwargs)
         await db.execute(query)
         await db.commit()
 
-    async def filter_update(self, db: AsyncSession, filters: list, **kwargs) -> None:
+    async def filter_update(self, db: Session, filters: list, **kwargs) -> None:
         query = update(self.model).where(*filters).values(**kwargs)
         await db.execute(query)
         await db.commit()
 
 
 class BaseDeleteRepository[T](BaseRepository[T]):
-    async def _delete_single(self, db: AsyncSession, id: int | str) -> None:
+    async def _delete_single(self, db: Session, id: int | str) -> None:
         stmt = delete(self.model).where(cast("ColumnElement[bool]", self.model.id == id))
         await db.execute(stmt)
         await db.commit()
 
-    async def _delete_multiple(self, db: AsyncSession, ids: list[int | str]) -> None:
+    async def _delete_multiple(self, db: Session, ids: list[int | str]) -> None:
         if not ids:
             return
 
@@ -86,7 +85,7 @@ class BaseDeleteRepository[T](BaseRepository[T]):
         await db.execute(stmt)
         await db.commit()
 
-    async def delete(self, db: AsyncSession, id: int | str | tuple[int | str] | list[int | str]) -> None:
+    async def delete(self, db: Session, id: int | str | tuple[int | str] | list[int | str]) -> None:
         if isinstance(id, (int, str)):
             await self._delete_single(db, id)
         elif isinstance(id, (tuple, list)) and len(id):
