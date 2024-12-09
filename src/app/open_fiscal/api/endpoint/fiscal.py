1d904cd5a4b514ef67341d008be4f96ce8231dfe
IIH <ds5105119@gmail.com>
2024-12-10 06:54:57 +0900

    add pagination response

--- src/app/open_fiscal/api/endpoint/fiscal.py
+++ src/app/open_fiscal/api/endpoint/fiscal.py
@@ -1,14 +1,14 @@
 from typing import Annotated
 
+import polars as pl
 from fastapi import APIRouter, Depends, status
-from fastapi_pagination.async_paginator import paginate
 
 from src.app.open_fiscal.api.dependencies import fiscal_service
-from src.core.utils.polarspagination import LazyFramePage
+from src.core.utils.polarspagination import LazyFramePage, paginate
 
 router = APIRouter()
 
 
 @router.get("/data", status_code=status.HTTP_200_OK)
-async def get_fiscal_data(data: Annotated[dict, Depends(fiscal_service.get_fiscal)]) -> LazyFramePage[dict]:
+async def get_fiscal_data(data: Annotated[pl.LazyFrame, Depends(fiscal_service.get_fiscal)]) -> LazyFramePage[dict]:
     return await paginate(data)
--- src/core/utils/polarspagination.py
+++ src/core/utils/polarspagination.py
@@ -1,12 +1,21 @@
-from typing import Any, Generic, TypeVar
+from typing import Any, Generic, Self, TypeVar
 
 import polars as pl
 from fastapi import Query
+from fastapi_pagination.api import apply_items_transformer, create_page
 from fastapi_pagination.bases import AbstractPage, AbstractParams, RawParams
-from pydantic import BaseModel
-from typing_extensions import Self
+from fastapi_pagination.types import AdditionalData, ItemsTransformer
+from pydantic import BaseModel, Field
 
 T = TypeVar("T", bound=pl.LazyFrame)
+
+
+def _get_lazf_length(dataset: pl.LazyFrame):
+    return dataset.select(pl.len()).collect().item()
+
+
+def _paginated_lazf(dataset: pl.LazyFrame, params: RawParams) -> list:
+    return dataset.slice(params.offset, params.limit).collect().to_dicts()
 
 
 class LazyFrameParams(BaseModel, AbstractParams):
@@ -17,30 +26,63 @@ class LazyFrameParams(BaseModel, AbstractParams):
         return RawParams(
             limit=self.size if self.size is not None else None,
             offset=self.size * (self.page - 1) if self.page is not None and self.size is not None else None,
-            include_total=False,
         )
 
 
 class LazyFramePage(AbstractPage[T], Generic[T]):
-    results: list[T]
-    totalResults: int
+    items: list[T]
+    total: int = Field(ge=0)
+    pageSize: int = Field(ge=0)
+    nextPage: int | None = Field(ge=1)
 
     __params_type__ = LazyFrameParams
 
-    @staticmethod
-    def get_lazf_length(dataset: pl.LazyFrame):
-        return dataset.select(pl.len()).collect().item()
-
-    @staticmethod
-    def paginated_lazf(dataset: pl.LazyFrame, params: RawParams) -> list:
-        return dataset.slice(params.offset, params.limit).collect().to_dicts()
-
     @classmethod
-    def create(cls, items: T, params: LazyFrameParams, *args, **kwargs: Any) -> Self:
-        total = cls.get_lazf_length(items)
-        items = cls.paginated_lazf(items, params.to_raw_params())
+    def create(cls, items: T, params: LazyFrameParams, *, total: int | None = None, **kwargs: Any) -> Self:
+        page_size = len(items)
+        next_offset = params.page * page_size
+
+        if next_offset >= total:
+            next_page = None
+        else:
+            next_page = params.page + 1
 
         return cls(
-            results=items,
-            totalResults=total,
+            total=total,
+            items=items,
+            pageSize=page_size,
+            nextPage=next_page,
         )
+
+
+def verify_params(params: LazyFrameParams | None) -> tuple[LazyFrameParams, RawParams]:
+    from fastapi_pagination.api import resolve_params
+
+    params = resolve_params(params)
+    raw_params = params.to_raw_params()
+
+    if raw_params.type != "limit-offset":
+        raise ValueError(f"{raw_params.type!r} params not supported")
+
+    return params, raw_params
+
+
+async def paginate(
+    sequence: T,
+    params: LazyFrameParams | None = None,
+    *,
+    transformer: ItemsTransformer | None = None,
+    additional_data: AdditionalData | None = None,
+) -> Any:
+    params, raw_params = verify_params(params)
+
+    items = _paginated_lazf(sequence, raw_params)
+    t_items = await apply_items_transformer(items, transformer, async_=True)
+    total = _get_lazf_length(sequence)
+
+    return create_page(
+        t_items,
+        total=total,
+        params=params,
+        **(additional_data or {}),
+    )
